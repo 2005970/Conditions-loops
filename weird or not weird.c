c54185b36d87352e2c7b388d33969d6c8b132978
chandana <chandana4871@gmail.com>
2025-11-11 21:05:44 +0530

    Create weird or not weird.c
    Given an integer, n, perform the following conditional actions:
    If n is odd, print weird
    If n is even and in the inclusive range of 2 to 5, print not weird
    If n is even and in the inclusive range of 6 to 20, print weird
    If n is even and greater than 20, print not weird

---
+++ weird or not weird.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main() {
+    int n;
+    scanf("%d", &n);
+
+    if (n % 2 != 0) {
+        printf("weird\n");
+    } 
+    else {
+        if (n >= 2 && n <= 5)
+            printf("Not weird\n");
+        else if (n >= 6 && n <= 20)
+            printf("weird\n");
+        else
+            printf("Not weird\n");
+    }
+
+    return 0;
+}
